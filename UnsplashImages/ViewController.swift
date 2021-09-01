dc900ff9e9cf2fc8d29f1e308a943f3f16083a61
Andrii Shkliaruk <devshkliaruk@gmail.com>
2021-09-01 18:04:28 +0300

    Unsplash API TestFlight

--- UnsplashImages/ViewController.swift
+++ UnsplashImages/ViewController.swift
@@ -7,13 +7,48 @@
 
 import UIKit
 
+struct APIResponse: Codable {
+    let results: [Result]
+}
+
+struct Result: Codable {
+    let id: String
+    let urls: URLS
+}
+
+struct URLS: Codable {
+    let full: String
+}
+
 class ViewController: UIViewController {
+    
+    let urlString = "https://api.unsplash.com/photos?per_page=30&client_id=-UBKtf-Pb9qNmEJR0mxnJsiR5NuAiMMyyKBnbkTlD6s"
+    
+    var results: [Result] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        loadData(url: urlString)
     }
-
+    
+    
+    func loadData(url: String) {
+        guard let url = URL(string: urlString) else { return }
+        let task = URLSession.shared.dataTask(with: url) { [weak self] data, _, error in
+            guard let data = data, error == nil else { return }
+            
+            do {
+                let jsonResult = try JSONDecoder().decode([Result].self, from: data)
+                self?.results = jsonResult
+                print(jsonResult.count)
+            }
+            catch {
+                print("ERROR - - - \(error)")
+            }
+        }
+        
+        task.resume()
+    }
 
 }
 
